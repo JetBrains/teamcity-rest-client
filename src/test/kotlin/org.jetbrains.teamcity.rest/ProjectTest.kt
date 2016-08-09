07c72e689cc536a9b3e8eb8aaaec01b10f7450ab
nik <Nikolay.Chashnikov@jetbrains.com>
2016-08-09 11:52:36 +0300

    tests added

--- src/test/kotlin/org.jetbrains.teamcity.rest/ProjectTest.kt
+++ src/test/kotlin/org.jetbrains.teamcity.rest/ProjectTest.kt
@@ -1,6 +1,7 @@
 package org.jetbrains.teamcity.rest
 
-import org.junit.Assert
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Before
 import org.junit.Test
 
@@ -13,12 +14,16 @@ class ProjectTest {
     @Test
     fun `project by id`() {
         val project = publicInstance().project(kotlinProject)
-        Assert.assertEquals("Kotlin", project.name)
+        assertEquals(kotlinProject, project.id)
+        assertEquals("Kotlin", project.name)
+        assertFalse(project.archived)
     }
 
     @Test
     fun `build configuration by id`() {
         val configuration = publicInstance().buildConfiguration(compilerAndPluginConfiguration)
-        Assert.assertEquals("Compiler and Plugin", configuration.name)
+        assertEquals(compilerAndPluginConfiguration, configuration.id)
+        assertEquals("Compiler and Plugin", configuration.name)
+        assertEquals(kotlinProject, configuration.projectId)
     }
 }
