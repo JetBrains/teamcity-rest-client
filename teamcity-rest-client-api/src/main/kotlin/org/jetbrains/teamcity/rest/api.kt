2fdd39f473432b3048905cbd01e58c893087fd94
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2018-03-31 14:32:52 +0300

    using `TeamCityRestException` instead of `java.lang.Error` when connection error occurs

--- teamcity-rest-client-api/src/main/kotlin/org/jetbrains/teamcity/rest/api.kt
+++ teamcity-rest-client-api/src/main/kotlin/org/jetbrains/teamcity/rest/api.kt
@@ -270,3 +270,9 @@ interface ArtifactRule {
      */
     val destinationPath: String?
 }
+
+open class TeamCityRestException(message: String?, cause: Throwable?) : RuntimeException(message, cause)
+
+open class TeamCityQueryException(message: String?, cause: Throwable? = null) : TeamCityRestException(message, cause)
+
+open class TeamCityConversationException(message: String?, cause: Throwable? = null) : TeamCityRestException(message, cause)
