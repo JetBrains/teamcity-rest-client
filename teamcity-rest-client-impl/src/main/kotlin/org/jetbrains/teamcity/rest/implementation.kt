a04c36432f3ef8cb1f4a042c7a05eb7201ed4e05
Aleksey Rostovskiy <aleksey.rostovskiy@jetbrains.com>
2018-05-24 15:21:16 +0300

    extend timeout for http's read-write operations

--- teamcity-rest-client-impl/src/main/kotlin/org/jetbrains/teamcity/rest/implementation.kt
+++ teamcity-rest-client-impl/src/main/kotlin/org/jetbrains/teamcity/rest/implementation.kt
@@ -14,6 +14,7 @@ import java.io.FileOutputStream
 import java.net.URLEncoder
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit.SECONDS
 
 private val LOG = LoggerFactory.getLogger("teamcity-rest-client")
 
@@ -59,6 +60,10 @@ internal class TeamCityInstanceImpl(internal val serverUrl: String,
 
     private var client = OkHttpClient.Builder()
             .addInterceptor(RetryInterceptor())
+            .also {
+                it.readTimeout(60, SECONDS)
+                it.writeTimeout(60, SECONDS)
+            }
             .build()
 
     internal val service = RestAdapter.Builder()
