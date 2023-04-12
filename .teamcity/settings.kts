b5875e5503351c23542acabd05ea41cfc248b72c
andrey.kokorev <andrey.kokorev@jetbrains.com>
2023-04-12 16:54:18 +0200

    Added same settings as in existing configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,29 +1,79 @@
 import jetbrains.buildServer.configs.kotlin.*
-
-/*
-The settings script is an entry point for defining a TeamCity
-project hierarchy. The script should contain a single call to the
-project() function with a Project instance or an init function as
-an argument.
-
-VcsRoots, BuildTypes, Templates, and subprojects can be
-registered inside the project using the vcsRoot(), buildType(),
-template(), and subProject() methods respectively.
-
-To debug settings scripts in command-line, run the
-
-    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
-
-command and attach your debugger to the port 8000.
-
-To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
--> Tool Windows -> Maven Projects), find the generate task node
-(Plugins -> teamcity-configs -> teamcity-configs:generate), the
-'Debug' option is available in the context menu for the task.
-*/
+import jetbrains.buildServer.configs.kotlin.buildSteps.gradle
+import jetbrains.buildServer.configs.kotlin.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.triggers.vcs
 
 version = "2022.10"
 
 project {
     description = "REST API client written in Kotlin"
+
+    buildType {
+        id("TC_TeamCityTools_TeamCityRestClient_Build")
+        name = "Build"
+        triggers {
+            vcs {
+                branchFilter = "+:<default>"
+            }
+        }
+        requirements {
+            exists("docker.version")
+            equals("teamcity.agent.jvm.os.name", "Linux")
+        }
+        params {
+            text("username", "admin")
+            password("password", "admin")
+        }
+
+        steps {
+            script {
+                name = "Deploy test data"
+                scriptContent = """
+                #!/bin/bash
+                DATA_DIR="${'$'}HOME/DataDirs/web-tests-rest"
+                mkdir -p ${'$'}DATA_DIR
+                wget -qO- "https://repo.labs.intellij.net/teamcity/org/jetbrains/teamcity/tc-rest-client-tests-db/1.1.0-SNAPSHOT/tc-rest-client-tests-db-1.1.0-20200123.155623-1.tar.gz" | tar xvz -C ${'$'}DATA_DIR
+                """.trimIndent()
+            }
+
+            script {
+                name = "Start server"
+                scriptContent = """
+                #!/bin/bash
+                docker run -d --name teamcity-server -v ${'$'}HOME/DataDirs/web-tests-rest:/data/teamcity_server/datadir -p 8111:8111 jetbrains/teamcity-server:2020.1.5
+                until $(curl --output /dev/null --silent --head --fail http://localhost:8111/login.html); do
+                echo "waiting for teamcity server startup completion..."
+                sleep 5
+                done
+                """.trimIndent()
+            }
+
+            script {
+                name = "Create teamcity_connection.properties"
+                workingDir = "teamcity-rest-client-impl/src/test"
+                scriptContent = """
+                mkdir resources
+                touch resources/teamcity_connection.properties
+                echo "serverUrl=http://localhost:8111" > resources/teamcity_connection.properties
+                echo "username=%username%" >> resources/teamcity_connection.properties
+                echo "password=%password%" >> resources/teamcity_connection.properties
+                """.trimIndent()
+            }
+
+            gradle {
+                name = "Build"
+                tasks = "build"
+            }
+
+            script {
+                name = "Stop server"
+                executionMode = BuildStep.ExecutionMode.ALWAYS
+                scriptContent = """
+                #!/bin/bash
+                docker stop teamcity-server
+                docker rm teamcity-server
+                """.trimIndent()
+            }
+        }
+    }
 }
