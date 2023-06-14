04944dd8a0d843dbbf841738636fc11997cc33f6
Bo Zhang <bo@gradle.com>
2023-06-14 20:20:32 +0800

    Add "affectedProject:(id:X)" build locator (#105)

--- teamcity-rest-client-api/src/main/kotlin/org/jetbrains/teamcity/rest/api.kt
+++ teamcity-rest-client-api/src/main/kotlin/org/jetbrains/teamcity/rest/api.kt
@@ -113,6 +113,8 @@ interface UserLocator {
 }
 
 interface BuildLocator {
+    fun forProject(projectId: ProjectId): BuildLocator
+
     fun fromConfiguration(buildConfigurationId: BuildConfigurationId): BuildLocator
 
     fun withNumber(buildNumber: String): BuildLocator
